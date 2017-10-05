076eaba65b0d52440012d36f3c8b0380aa77f8fe
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-05 12:27:24 -0400

    Fixed panel navigation

--- static/js/custom.js
+++ static/js/custom.js
@@ -17,10 +17,10 @@ function updateSubNav() {
     var $mainNav = $('#nav');
     var $secondaryNav = $('#nav_sub');
     var $secondaryNavContent = $('#subNav-content');
-    var $subNavContent = $mainNav.find('.menuItem.js-isActive .menuItem-children');
+    var $subNavContent = $mainNav.find('.js-showInRightPanel .menuItem-children');
 
 
-    if( $subNavContent.length > 0 ) {
+    if( $subNavContent.length > 0 && $subNavContent.closest('')) {
         $secondaryNavContent.html( $subNavContent.html() );
         $secondaryNav.removeClass('noContent');
     } else {
@@ -37,5 +37,3 @@ function navInit() {
 $(function(){
     navInit();
 });
-
-
