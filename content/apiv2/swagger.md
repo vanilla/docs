6901f47705599327ff9e416e301eb052893cffe0
Stéphane LaFlèche <stephane.l@vanillaforums.com>
2017-10-06 10:21:00 -0400

    Added swagger docs page

---
+++ content/apiv2/swagger.md
@@ -0,0 +1,11 @@
+---
+title: End Points
+tags: ["API", "APIv2", "Swagger"]
+category: "apiv2"
+swagger: true
+menu:
+  apiv2:
+    weight: 10
+---
+
+{{% swagger %}}
---
+++ static/js/swagger/swagger-ui-plugin.js
@@ -0,0 +1,36 @@
+jQuery(document).ready(function($) {
+    var HideTopbarPlugin = function () {
+        // this plugin overrides the Topbar component to return nothing
+        return {
+            components: {
+                Topbar: function() { return null }
+            }
+        }
+    }
+
+    window.ui = SwaggerUIBundle({
+        url: '/js/swagger/swagger-data.json',
+        dom_id: '#swagger-ui',
+        deepLinking: true,
+        presets: [
+            SwaggerUIBundle.presets.apis,
+            SwaggerUIStandalonePreset
+        ],
+        plugins: [
+            SwaggerUIBundle.plugins.DownloadUrl,
+            HideTopbarPlugin,
+        ],
+        layout: "StandaloneLayout",
+        validatorUrl: null,
+        // filter: true,
+        requestInterceptor: function (request) {
+            request.headers['x-transient-key'] = gdn.getMeta('TransientKey');
+        },
+        onComplete: function(swaggerApi, swaggerUi){
+            $('$swagger-ui .try-out, #swagger-ui .information-container').each(function() {
+                $(this).hide();
+            })
+        }
+    });
+
+});
