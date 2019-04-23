cf60b5b7fd9c06b50bcf4d40527ab6601b114e51
Adam Charron <adam@charrondev.com>
2019-04-23 17:28:41 -0400

    Fix swagger post bodies

--- static/js/swagger/swagger-ui-plugin.js
+++ static/js/swagger/swagger-ui-plugin.js
@@ -18,25 +18,7 @@ jQuery(document).ready(function($) {
         url: '/js/swagger/swagger-data.json',
         dom_id: '#swagger-ui',
         deepLinking: true,
-        presets: [
-            SwaggerUIBundle.presets.apis,
-            SwaggerUIStandalonePreset
-        ],
-        plugins: [
-            SwaggerUIBundle.plugins.DownloadUrl,
-            HideTopbarPlugin
-        ],
-        layout: "StandaloneLayout",
         validatorUrl: null,
-        // filter: true,
-        requestInterceptor: function (request) {
-            request.headers['x-transient-key'] = gdn.getMeta('TransientKey');
-        },
-        onComplete: function(swaggerApi, swaggerUi){
-            $('$swagger-ui .try-out, #swagger-ui .information-container').each(function() {
-                $(this).hide();
-            })
-        }
     });
 
 });
